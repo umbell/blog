d88ab464ffa01a0c058c067abe864f794b049b5d
dcx <dcxtop@qq.com>
2023-03-12 16:10:41 +0800

    前端

---
+++ blog-app/src/components/article/ArticleItem.vue
@@ -0,0 +1,122 @@
+<template>
+  <el-card class="me-area" :body-style="{ padding: '16px' }">
+    <div class="me-article-header">
+
+      <a @click="view(id)" class="me-article-title">{{title}}</a>
+      <el-button v-if="weight > 0" class="me-article-icon" type="text">置顶</el-button>
+      <span class="me-pull-right me-article-count">
+	    	<i class="me-icon-comment"></i>&nbsp;{{commentCounts}}
+	    </span>
+      <span class="me-pull-right me-article-count">
+	    	<i class="el-icon-view"></i>&nbsp;{{viewCounts}}
+	    </span>
+    </div>
+
+    <div class="me-artile-description">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{summary}}
+    </div>
+
+    <div class="me-article-footer">
+<!-- 	  	<span class="me-article-author">
+	    	<i class="me-icon-author"></i>&nbsp;{{author}}
+	    </span> -->
+
+      <el-tag v-for="t in tags" :key="t.tagName" size="mini" type="success">{{t.tagName}}</el-tag>
+
+      <span class="me-pull-right me-article-count">
+	    	<i class="el-icon-time"></i>&nbsp;{{createDate | format}}
+	    </span>
+
+    </div>
+
+
+</el-card>
+
+
+
+
+
+
+</template>
+
+<script>
+  import { formatTime } from "../../utils/time";
+
+  export default {
+    name: 'ArticleItem',
+    props: {
+      id: String,
+      weight: Number,
+      title: String,
+      commentCounts: Number,
+      viewCounts: Number,
+      summary: String,
+      author: String,
+      tags: Array,
+      createDate: String
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      view(id) {
+        this.$router.push({path: `/view/${id}`})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .me-area{
+  border-radius: 12px;
+  }
+  .me-article-header {
+    /*padding: 10px 18px;*/
+    padding-bottom: 10px;
+  }
+
+  .me-article-title {
+    font-weight: 700;
+
+  }
+
+  .me-article-icon {
+    padding: 3px 8px;
+  }
+
+  .me-article-count {
+    color: #a6a6a6;
+    padding-left: 14px;
+    font-size: 13px;
+  }
+
+  .me-pull-right {
+    float: right;
+  }
+
+  .me-artile-description {
+    font-family: "行书";
+    font-size: 13px;
+    line-height: 24px;
+    margin-bottom: 10px;
+  }
+
+  .me-article-author {
+    color: #a6a6a6;
+    padding-right: 18px;
+    font-size: 13px;
+  }
+
+  .el-tag {
+    margin-right: 8px;
+    margin-top: 8px;
+  }
+ .me-article-footer{
+    padding-bottom: 8px;
+  }
+  .me-article-count{
+    margin-top: 15px;
+  }
+
+</style>
---
+++ blog-app/src/components/card/CardMe.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-card>
+    <h1 class="me-author-name">DCXIN.TOP</h1>
+    <div class="me-author-description">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <span><i class="el-icon-location-outline"></i> &nbsp;怀揣星辰，砥砺前行</span>
+    </div>
+    <div class="me-author-tool">
+      <i @click="showTool(qq)" :title="qq.title" class="iconfont icon-qq"></i><br>
+      <span>528654065@qq.com</span>
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: 'CardMe',
+    data() {
+      return {
+        qq: {title: 'QQ', message: '528654065'},
+      }
+    },
+    methods: {
+      showTool(tool) {
+        this.$message({
+          duration: 0,
+          showClose: true,
+          dangerouslyUseHTMLString: true,
+          message: '<strong>' + tool.message + '</strong>'
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .me-author-name {
+    text-align: center;
+    font-size: 30px;
+    border-bottom: 1px solid #5FB878;
+  }
+
+  .me-author-description {
+    padding: 8px 0;
+  }
+
+  .me-icon-job {
+    padding-left: 16px;
+  }
+
+  .me-author-tool {
+    text-align: center;
+    padding-top: 50px;
+    padding-top: 10px;
+  }
+
+  .me-author-tool i {
+    cursor: pointer;
+    font-size: 30px;
+  }
+</style>
